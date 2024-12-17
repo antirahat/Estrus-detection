fd796fae0a8a4a4ad958387534388fadcda56b66
Md. Rahat Al Mamun <107720172+antirahat@users.noreply.github.com>
2024-12-18 03:42:20 +0600

    Update rfe.py

--- dataset and code/rfe.py
+++ dataset and code/rfe.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report,confusion_matrix
+import seaborn as sns
 
 
 #label filter all the dataset
@@ -62,3 +63,11 @@ y_pred = rfe.predict(x_test)
 print("Random Forest Ensemble:")
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print("Classification Report:\n", classification_report(y_test, y_pred))
+
+# Confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=sorted(y.unique()), yticklabels=sorted(y.unique()))
+plt.title("Confusion Matrix for Random Forest Ensemble")
+plt.xlabel("Predicted")
+plt.ylabel("True")
+plt.show()
